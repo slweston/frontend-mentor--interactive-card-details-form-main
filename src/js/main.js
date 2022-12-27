9c1b16ebb6f15fa0cd77878e828ee492cfe498b4
Scott Weston <swest2020-github@yahoo.com>
2022-12-27 14:34:20 -0600

    changed continue button to reload url.

--- src/js/main.js
+++ src/js/main.js
@@ -52,7 +52,7 @@
 	{
 		const continueButton = document.querySelector('.continue-button');
 		continueButton.addEventListener('click', (e) => {
-			location.reload();
+			location.href = location.href;
 		});
 	}
 })();
